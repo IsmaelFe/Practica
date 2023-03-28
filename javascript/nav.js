527ebb1dd48a13ea11ef3858ce8a56229589e071
Abner Ismael Sul Felipe <abnersul80@gmail.com>
2023-03-27 19:57:07 -0600

    Agregando funcionalidad al menú responsive

---
+++ javascript/nav.js
@@ -0,0 +1,10 @@
+const newClass = 'bar-nav-visible';
+const actualClass = 'bar-nav'
+
+function cambioClassP() {
+    document.getElementById('nav').className = newClass;
+}
+
+function cambioClassS() {
+    document.getElementById('nav').className = actualClass;
+} 
